53a3ea7c86b6de6eef50ad83e6771236533eb51c
isaac-svg <isaacsakyi009@gmail.com>
2024-05-09 12:44:23 +0000

    done

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -3,32 +3,33 @@
 Calculates the fewest number of operations needed
 to result in exactly n H characters in the file.
 """
+
+
 def minOperations(n):
     """
     Calculates the fewest number of operations needed
     to result in exactly n H characters in the file.
-    
+
     Args:
     - n (int): The target number of H characters.
-    
+
     Returns:
-    - int: The fewest number of operations needed. If n is impossible to achieve, returns 0.
+    - int: The fewest number of operations needed.
+      If n is impossible to achieve, returns 0.
     """
 
     if n == 0:
         return 0
-    
+
     operations = 0
     factor = 2
-    
+
     while n > 1:
-        
+
         while n % factor == 0:
             n //= factor
             operations += factor
-        
+
         factor += 1
-        
+
     return operations
-
-
