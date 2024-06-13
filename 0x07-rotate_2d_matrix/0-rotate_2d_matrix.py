cf7cff1002fde448cce949226946d52943f5e4b9
isaac-svg <isaacsakyi009@gmail.com>
2024-06-13 20:16:46 +0000

    task 7

---
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Matrix manipulation"""
+
+
+def rotate_2d_matrix(matrix):
+    """
+    Rotates a 2d matrix 90 degrees clockwise
+    Returns: Nothing
+    """
+    left, right = 0, len(matrix) - 1
+
+    while left < right:
+        for i in range(right - left):
+            top, bottom = left, right
+            topLeft = matrix[top][left + i]
+            matrix[top][left + i] = matrix[bottom - i][left]
+            matrix[bottom - i][left] = matrix[bottom][right - i]
+            matrix[bottom][right - i] = matrix[top + i][right]
+            matrix[top + i][right] = topLeft
+        right -= 1
+        left += 1
