c234f7845d3ef4dc43e7c9941f1d1f78ddbc2a01
isaac-svg <isaacsakyi009@gmail.com>
2024-06-20 20:39:47 +0000

    interview preaparation 8

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+'''Interview Preparation
+'''
+import sys
+
+
+def makeChange(coins, total):
+    '''
+    Return: fewest number of coins needed to meet total
+
+    '''
+    if total <= 0:
+        return 0
+    table = [sys.maxsize for i in range(total + 1)]
+    table[0] = 0
+    m = len(coins)
+    for i in range(1, total + 1):
+        for j in range(m):
+            if coins[j] <= i:
+                subres = table[i - coins[j]]
+                if subres != sys.maxsize and subres + 1 < table[i]:
+                    table[i] = subres + 1
+
+    if table[total] == sys.maxsize:
+        return -1
+    return table[total]
